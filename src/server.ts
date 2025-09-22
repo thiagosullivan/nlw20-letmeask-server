dae253502f331b6b7828c786b0505f38d0b0622a
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-22 19:11:00 -0300

    chore: setting up the project

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+  return 'OK';
+});
+
+app.listen({ port: env.PORT });
