e5fbadde551dad8ade425dc42a1b0625a244693a
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-23 00:35:28 -0300

    feat: add drizzle

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,10 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
+import { sql } from './db/connection.ts';
 import { env } from './env.ts';
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
